1a037b14ca2a30fb517fb4967f536d6e2bdf5cb8
Rajasree <me.shree@gmail.com>
2018-04-09 23:10:28 -0700

    Finishing touches added to JustJava app !

--- just_java/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ just_java/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -41,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        int price = quantity *5;
+        String priceMessage = "Total: $" + price + "\n Thank you!";
+        displayMessage (priceMessage);
     }
 
     /**
@@ -57,5 +59,13 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
